8b832c4e3bd7be3f25765a595552f64cb4a3a6ca
dolphima <veranda.dp@gmail.com>
2023-09-20 12:06:56 +0300

    add styles for block reviews

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ $(function () {
     dots: true,
     arrows: true,
     speed: 500,
-    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><svg svg width="11" height="19" viewBox="0 0 11 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.19676 18.0492L0.450821 10.3033C-0.150273 9.69905 -0.150273 8.72311 0.450821 8.11889L8.19676 0.372949C8.77613 -0.124317 9.63285 -0.124317 10.2122 0.372949C10.8614 0.929121 10.9372 1.90818 10.3811 2.55732L3.73512 9.20334L10.3811 15.8648C10.9822 164.469 10.9822 17.445 10.3811 18.0492C9.77691 18.6503 8.8009 18.6503 8.19676 18.0492Z" fill="#C2C2C2"/></svg></button>',
-    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><svg svg width="11" height="19" viewBox="0 0 11 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.19676 18.0492L0.450821 10.3033C-0.150273 9.69905 -0.150273 8.72311 0.450821 8.11889L8.19676 0.372949C8.77613 -0.124317 9.63285 -0.124317 10.2122 0.372949C10.8614 0.929121 10.9372 1.90818 10.3811 2.55732L3.73512 9.20334L10.3811 15.8648C10.9822 16.469 10.9822 17.445 10.3811 18.0492C9.77691 18.6503 8.8009 18.6503 8.19676 18.0492Z" fill="#C2C2C2"/></svg></button>',
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><svg xmlns="http://www.w3.org/2000/svg" width="11" height="19" fill="none"><path d="M8.197 18.05.45 10.302a1.548 1.548 0 0 1 0-2.184L8.197.373a1.548 1.548 0 0 1 2.184 2.184L3.735 9.203l6.646 6.662c.601.604.601 1.58 0 2.184a1.548 1.548 0 0 1-2.184 0Z"/></svg></button>',
+    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><svg xmlns="http://www.w3.org/2000/svg" width="11" height="19" fill="none"><path d="M8.197 18.05.45 10.302a1.548 1.548 0 0 1 0-2.184L8.197.373a1.548 1.548 0 0 1 2.184 2.184L3.735 9.203l6.646 6.662c.601.604.601 1.58 0 2.184a1.548 1.548 0 0 1-2.184 0Z"/></svg></button>',
   });
 });
 
